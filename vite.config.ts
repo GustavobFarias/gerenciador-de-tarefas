a8af445318fbb22d44881d06ed2f3ace2473ff37
Gustavo <gustavoburgerf@gmail.com>
2025-10-16 18:35:20 -0300

    Ajuste no build

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    outDir: 'build',
+    outDir: 'dist',
     reportCompressedSize: false, // 🚀 Desativa cálculo gzip (evita travamento)
   },
   server: {
